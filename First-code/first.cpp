22bd750366dfb3a3a77e52e3ad8471b7e3797f5b
Krrishjaiswal <krrishjais45678@gmail.com>
2026-01-23 17:21:58 +0000

    Added-Week-1-Learning

---
+++ First-code/first.cpp
@@ -0,0 +1,11 @@
+// First Code
+
+// Header file: using for input/output code
+#include <iostream>
+// removing std for repetation
+using namespace std;
+
+//Starr
+int main(){
+   cout<<"krrish jaiswal";
+} 
---
+++ Variable-&-Datatype/int.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a; // Datatype Declaration 
+    a = 10; // Value declaration 
+    cout<<a; // print
+    int b = 4;
+    cout<<b;
+}
+
