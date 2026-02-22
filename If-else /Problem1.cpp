caf6a95e82504129faa3ada2cdc3512091afb243
Krrishjaiswal <krrishjais45678@gmail.com>
2026-02-22 17:49:19 +0000

    Added-CPP-Learnings

---
+++ If-else /Problem1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    int pacakage;
+    cout << "value of pacakage : ";
+    cin >> pacakage;
+
+    if(pacakage>10){
+        cout<<"Accepted";
+    } else
+    cout<<"REjected";
+}
