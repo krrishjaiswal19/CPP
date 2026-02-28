8eda8c2463a86c3cb381676da971081b6b343abd
Krrishjaiswal <krrishjais45678@gmail.com>
2026-02-28 17:35:30 +0000

    Loops-problems

---
+++ Loops/doWhile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    int i=1; // intialize
+    do{
+      cout<<i<<endl;
+      i++; // update
+    }while(i<=n); // break
+}
---
+++ Loops/whileLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int i=1; // initalize
+
+    while(i<=10){ // Break
+        cout<<i*n<<endl;
+        i++; //update
+    }
+}
